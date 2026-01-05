5f00455fcd7704c0d75a672e1f7567f777e55edc
Paola Novick <paola.novick@davinci.edu.ar>
2026-01-05 15:31:01 -0300

    agrego friends a la home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/home/Hero";
 import Categories from "../components/home/Categories";
+import OurFriends from "../components/home/OurFriends";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-16">
       <Hero />
       <Categories />
+      <OurFriends /> 
     </div>
   );
 }
